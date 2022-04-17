e07fb33ca632c6960ffb8a8d8a4a80b911a227ab
gokayari <gokayari@gmail.com>
2022-04-17 18:13:24 +0200

    Protected Access Modifier

--- src/utilities/AccessModifiers_Test1.java
+++ src/utilities/AccessModifiers_Test1.java
@@ -2,7 +2,7 @@ package utilities;
 
 import day34_GarbageCollection_AccessModifiers.AccessModifiers;
 
-public class AccessModifiers_Test {
+public class AccessModifiers_Test1 {
 
     public static void main(String[] args) {
 
