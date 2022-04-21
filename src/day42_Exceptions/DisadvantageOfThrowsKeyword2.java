67c3084206debc7b3f8aba33cb0d0476985c32a0
gokayari <gokayari@gmail.com>
2022-04-22 01:02:04 +0200

    Exceptions Continue

---
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
@@ -0,0 +1,35 @@
+package day42_Exceptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class DisadvantageOfThrowsKeyword2 {
+
+    public static void method1() throws FileNotFoundException {
+
+        new FileInputStream("");
+    }
+
+    public static void method2() throws FileNotFoundException {
+
+        method1();
+
+    }
+
+    public static void method3(){
+
+        //method2();
+
+        try {
+            method1();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void method4(){
+
+        method3();
+    }
+
+}
