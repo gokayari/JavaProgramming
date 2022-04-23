a9afe61bd7a87713bf32bdaaaede0bed7c35531a
gokayari <gokayari@gmail.com>
2022-04-23 17:16:51 +0200

    liveReview week15 - Exceptions

---
+++ liveReview/week15/TryCatchExamples.java
@@ -0,0 +1,16 @@
+package week15;
+
+public class TryCatchExamples {
+
+    public static void main(String[] args) {
+
+        try{
+            System.out.println(10 / 2);
+            System.out.println(10 / 0);
+            System.out.println(10 / 3); //this line is skipped
+        }catch (ArithmeticException arithmeticException){
+            System.out.println("Arithmetic Exception is handled");  //for info
+            //System.out.println(arithmeticException.getMessage()); //for info-2
+        }
+    }
+}
