c2b40098e79707a06eca60a0d3f0aa45d59e961f
gokayari <gokayari@gmail.com>
2022-04-23 18:34:52 +0200

    liveReview week15 - Exceptions

---
+++ liveReview/week15/customExceptions/InsufficientBalanceException.java
@@ -0,0 +1,13 @@
+package week15.customExceptions;
+
+public class InsufficientBalanceException extends RuntimeException {
+
+    public InsufficientBalanceException() {
+    }
+
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+
+
