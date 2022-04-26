94520d0813d1fcd1f9140b5b81f56558239fc57b
gokayari <gokayari@gmail.com>
2022-04-26 15:09:45 +0200

    Abstraction - Sub classes

--- src/day43_Abstraction/car/Tesla.java
+++ src/day43_Abstraction/car/Tesla.java
@@ -1,6 +1,6 @@
 package day43_Abstraction.car;
 
-public class Tesla extends Car{
+public final class Tesla extends Car{ //final class for autoPilot
 
     public Tesla(String brand, String model, String color, int year, double price) {
         super("Tesla", model, color, year, price);
@@ -8,5 +8,9 @@ public class Tesla extends Car{
 
     public void start(){
         System.out.println("Say \"Start\"");
+    }
+
+    public void autoPilot(){
+        System.out.println(getBrand() + " " + getModel() + " has Auto Pilot feature");
     }
 }
