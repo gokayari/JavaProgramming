8c1215664b9c2660da8e5fd0994f20183fe7d396
gokayari <gokayari@gmail.com>
2022-04-26 15:23:37 +0200

    Abstraction - Sub classes

--- src/day43_Abstraction/car/Honda.java
+++ src/day43_Abstraction/car/Honda.java
@@ -3,7 +3,7 @@ package day43_Abstraction.car;
 public class Honda extends Car{
                     //it CANNOT be abstract class, otherwise we cannot create objects
 
-    public Honda(String brand, String model, String color, int year, double price) {
+    public Honda(String model, String color, int year, double price) {
         super("Honda", model, color, year, price);
     }
 
--- src/day43_Abstraction/car/Tesla.java
+++ src/day43_Abstraction/car/Tesla.java
@@ -2,7 +2,7 @@ package day43_Abstraction.car;
 
 public final class Tesla extends Car{ //final class for autoPilot
 
-    public Tesla(String brand, String model, String color, int year, double price) {
+    public Tesla(String model, String color, int year, double price) {
         super("Tesla", model, color, year, price);
     }
 
