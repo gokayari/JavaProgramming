5fa2a9694a3d865038f47a12aab5606aae5bc979
gokayari <gokayari@gmail.com>
2022-05-07 11:24:14 +0200

    day46 Polymorphism - Intro

---
+++ src/day46_Polymorphism/PolymorphismIntro.java
@@ -0,0 +1,107 @@
+package day46_Polymorphism;
+
+import day43_Abstraction.person.*;
+import day44_Abstraction.animalTask.Animal;
+import day44_Abstraction.animalTask.Cat;
+import day44_Abstraction.animalTask.Dog;
+import day44_Abstraction.animalTask.Playable;
+import day45_Abstraction.shape.*;
+
+import java.util.ArrayList;
+
+public class PolymorphismIntro {
+
+    public static void main(String[] args) {
+
+        String str = "Wooden Spoon";
+
+        Integer n1 = 2;
+
+        Double n2 = 5.5;
+
+        Boolean r1 = true;
+
+        Object[] array = {str, n1, n2, r1, new Circle(4), new Square(7)};
+
+        Tester tester = new Tester("Ali",30,'M',42,"SDET",145000.00);
+        Developer developer = new Developer("Alex",28,'M',32,"Web Developer",155000.00);
+        Teacher teacher = new Teacher("Angel",28,'F',22,"English Teacher",85000.00);
+        Driver driver = new Driver("John",34,'M',42,"Lyft Driver",75000.00);
+
+        ArrayList<Person> cydeoEmployees = new ArrayList();
+
+        cydeoEmployees.add(tester);
+        cydeoEmployees.add(developer);
+        cydeoEmployees.add(teacher);
+        cydeoEmployees.add(driver);
+
+        System.out.println("-----------------------");
+
+        Employee employee = new Tester("Ali",30,'M',42,"SDET",145000.00);
+        employee.work();
+        //employee.bugReport();     //bugReport reference type is not for all of employee
+
+        /*
+        WebDriver driver1 = new ChromeDriver();
+        WebDriver driver2 = new ChromeDriver();
+
+        driver1.get(amazon);
+        driver2.get(google);
+
+
+
+        Animal animal = new Dog();
+        animal.drink();
+
+        Animal animal = new Cat();
+        animal.drink();
+
+
+         */
+
+        String string = "Java";
+
+
+        Animal animal = new Dog("Max", "Husky", 'M', 3, "Small", "White");
+        animal.drink();
+        animal.eat();
+        //animal.play();    //play reference type is not for all of animal
+
+        Playable animal2 = new Dog("Max", "Husky", 'M', 3, "Small", "White");
+        System.out.println(animal2.isFriendly);
+        animal2.play();
+        //animal2.drink();
+        //animal2.eat();
+
+        Animal animal3 = new Cat("Max", "Husky", 'M', 3, "Small", "White");
+
+        Shape shape = new Circle(5);
+        shape.area();   //pi*r*r
+
+        //System.out.println(shape.getRadius());
+        //System.out.println(shape.PI);
+
+        boolean isSquare = shape instanceof Square;
+        System.out.println("isSquare = " + isSquare);   //false
+        boolean isRectangle = shape instanceof Rectangle;   //false
+        boolean isTriangle = shape instanceof Triangle;     //false
+        boolean isCircle = shape instanceof Circle;     //TRUE
+
+
+
+
+        /*
+        driver:
+
+            if(driver instanceof Chrome){
+                Chrome browser will open
+            }else if(driver instanceof firefox){
+                Firefox browser will open
+            }
+
+
+         */
+
+
+    }
+}
