1ab53094502401197fb527f7d3a2b747a7c21955
gokayari <gokayari@gmail.com>
2022-05-17 15:43:46 +0200

    day49 Collections - Queue

---
+++ src/day49_Collections/QueuePractice.java
@@ -0,0 +1,57 @@
+package day49_Collections;
+
+import java.util.*;
+
+public class QueuePractice {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        System.out.println("priorityQueue = " + priorityQueue);
+
+        Queue<Integer> arrayDeque = new ArrayDeque<>();
+        arrayDeque.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        System.out.println("arrayDeque = " + arrayDeque);
+
+        Queue<Integer> linkedList = new LinkedList<>();
+        linkedList.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        System.out.println("linkedList = " + linkedList);
+
+
+        System.out.println("-----------------------------------");
+
+
+        priorityQueue.poll();
+
+        System.out.println("priorityQueue = " + priorityQueue);
+
+        priorityQueue.poll();
+        System.out.println("priorityQueue = " + priorityQueue);
+
+
+        arrayDeque.poll();
+
+        System.out.println("arrayDeque = " + arrayDeque);
+
+        linkedList.poll();
+
+        System.out.println("linkedList = " + linkedList);
+
+        System.out.println("---------------------");
+
+        // System.out.println(priorityQueue.get(1));    //queue does not have INDEX NUMBER!!
+        // System.out.println(arrayDeque.get(1));      //queue does not have INDEX NUMBER!!
+        // System.out.println(linkedList.get(1) );      //queue does not have INDEX NUMBER!!
+
+        System.out.println( ( (List)linkedList ).get(1) );  //when reference type is changed to List
+
+        System.out.println(((LinkedList<Integer>) linkedList).get(1));
+
+
+
+    }
+}
