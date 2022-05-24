31e14f286041834384b566b8f54bbf67c26846fa
gokayari <gokayari@gmail.com>
2022-05-24 14:33:05 +0200

    day51 Map - Map Practice1, task1

---
+++ src/day51_Map/MapPractice1.java
@@ -0,0 +1,54 @@
+package day51_Map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapPractice1 {
+
+    public static void main(String[] args) {
+
+        Map<String, String> employeeMap = new HashMap<>();
+        employeeMap.put("Ddene", "F");
+        employeeMap.put("Winfred", "M");
+        employeeMap.put("Jereme", "M");
+        employeeMap.put("Glad", "F");
+        employeeMap.put("Desirae", "F");
+        employeeMap.put("Kakalina", "F");
+        employeeMap.put("Bertrando", "M");
+        employeeMap.put("Ajay", "F");
+        employeeMap.put("Brigitte", "F");
+        employeeMap.put("Derk", "M");
+        employeeMap.put("Orlando", "M");
+        employeeMap.put("Selle", "F");
+        employeeMap.put("Marika", "F");
+
+        //  1. Update the "M" to Male and "F" to Female
+        //solution#1:
+        /*
+        for (String key : employeeMap.keySet()) {
+            if (employeeMap.get(key).equalsIgnoreCase("m")){
+                employeeMap.replace(key, "Male");
+            }
+            if (employeeMap.get(key).equalsIgnoreCase("f")){
+                employeeMap.replace(key, "Female");
+            }
+        }
+         */
+
+        //solution#2:
+        for (Map.Entry<String, String> entry : employeeMap.entrySet()) {
+            if (entry.getValue().equalsIgnoreCase("M")){
+                entry.setValue("Male");     //easier
+                //employeeMap.replace(entry.getKey(), "Male");
+            }
+            if (entry.getValue().equalsIgnoreCase("F")){
+                entry.setValue("Female");     //easier
+                //employeeMap.replace(entry.getKey(), "Male");
+            }
+        }
+
+        System.out.println(employeeMap);
+
+        System.out.println("-------------------------");
+    }
+}
