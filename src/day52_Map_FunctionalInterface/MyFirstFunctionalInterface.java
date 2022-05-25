b0b7fcc6b1eac660e35d20ec60b2e42c59b1a59b
gokayari <gokayari@gmail.com>
2022-05-25 14:31:52 +0200

    day52 FunctionalInterface - First Functional Interface

---
+++ src/day52_Map_FunctionalInterface/MyFirstFunctionalInterface.java
@@ -0,0 +1,9 @@
+package day52_Map_FunctionalInterface;
+
+@FunctionalInterface
+public interface MyFirstFunctionalInterface {
+
+    void apply(int n);
+
+    //void function();  //it can not contain more than one abstract method
+}
