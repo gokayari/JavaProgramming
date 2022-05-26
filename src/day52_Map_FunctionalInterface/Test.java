c4cb0147813e066dbc691491f26600f0affba985
gokayari <gokayari@gmail.com>
2022-05-26 14:56:55 +0200

    day52 FunctionalInterface - Test Function2

--- src/day52_Map_FunctionalInterface/Test.java
+++ src/day52_Map_FunctionalInterface/Test.java
@@ -20,6 +20,15 @@ public class Test {
 
         //function2: crate a function that can check if a person is eligible
 
+        MyFirstFunctionalInterface eligibleToBuyAlcohol = (age) -> {
+            if (age>= 21){
+                System.out.println("You are eligible to buy alcohol");
+            }else{
+                System.out.println("You are NOT eligible to buy alcohol");
+            }
+        };
+        eligibleToBuyAlcohol.apply(19);
+
 
         //function3: create a function that can display the cube of number
     }
