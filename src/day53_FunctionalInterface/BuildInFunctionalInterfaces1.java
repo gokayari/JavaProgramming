73b21fc7ecf51a6355d6638bbd42bd59a8a0906f
gokayari <gokayari@gmail.com>
2022-06-05 12:59:14 +0200

    day53 FunctionalInterface - BuildInFunctionalInterfaces1 (Consumer)

--- src/day53_FunctionalInterface/BuildInFunctionalInterfaces1.java
+++ src/day53_FunctionalInterface/BuildInFunctionalInterfaces1.java
@@ -3,6 +3,7 @@ package day53_FunctionalInterface;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class BuildInFunctionalInterfaces1 {
@@ -41,6 +42,42 @@ public class BuildInFunctionalInterfaces1 {
         names.removeIf(isPalindrome);
 
         System.out.println(names);
+
+
+        System.out.println("---------------------");
+
+        //Consumer:
+
+        Consumer<String> printEach = (s) -> {
+            for (int i = 0; i < s.length(); i++){
+                System.out.println(s.charAt(i));
+            }
+        };
+
+        printEach.accept("Java");
+
+        System.out.println("----------------------");
+
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+        /*
+        for (Integer each : list2) {
+            if(each % 2 != 0){
+                System.out.println(each);
+            }
+        }
+         */
+        list2.forEach( p -> { if(p%2==0) System.out.println(p);});
+
+        System.out.println("------------------------");
+
+        List<String> employees = new ArrayList<>();
+        employees.addAll(Arrays.asList("Anel Adilbayeva", "Aygun Abbasova Jarullah",
+                "Jilili Jiwuer", "Ali Ismayilov"));
+
+        employees.forEach(
+                s -> System.out.println(s.charAt(0) + "." + s.charAt(s.lastIndexOf(" ")+1) ) );
 
 
     }
