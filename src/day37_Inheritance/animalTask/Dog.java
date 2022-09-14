4acd62d68881d4f0b080b911a85f296881543961
gokayari <gokayari@gmail.com>
2022-09-14 10:30:36 +0200

    day37 Inheritance - animalTask: super() and this()

--- src/day37_Inheritance/animalTask/Dog.java
+++ src/day37_Inheritance/animalTask/Dog.java
@@ -2,11 +2,14 @@ package day37_Inheritance.animalTask;
 
 public class Dog extends Animal{
 
-//  public int a;       //intance variable from sub class (this class= dog)
+//  public int a;       //instance variable from sub class (this class= dog)
                                                                                     //..., int a){
     public Dog(String name, String breed, char gender, int age, String size, String color){
-        super(name, breed, gender, age, size, color);   //instance vaiables from super class (Animal)
+        super(name, breed, gender, age, size, color);   //instance variables from super class (Animal)
 //      this.a = a;
+
+//      this() //=will call a constructor from another constructor within the same class
+//      //*** super() and this() can not be in the same constructor!
     }
 
     public void bark(){
